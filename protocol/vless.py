0afa653686746d8d72955488ab9eba76fc70e4ad
parsa-ab02 <parsaabedi02@gmail.com>
2026-08-09 21:03:13 +0330

    delete url handler and migrate t

--- protocol/vless.py
+++ protocol/vless.py
@@ -1,4 +1,4 @@
-from service.parser import ParsedURL
+from service.proxy import Proxy
 
 
 class VlessProxy:
@@ -7,32 +7,28 @@ class VlessProxy:
         "path", "host", "serviceName", "alpn", "mode"
     }
 
-    def __init__(self, parse_result: ParsedURL):
-        self.protocol = parse_result.scheme
-        self.uuid = parse_result.username
-        self.server = parse_result.hostname
-        self.port = parse_result.port
-        self.tag = parse_result.fragment
+    def __init__(self, proxy: Proxy):
+        self.proxy = proxy 
 
-        self.type = parse_result.get_param("type", "tcp")
-        self.security = parse_result.get_param("security", "none")
-        self.sni = parse_result.get_param("sni")
-        self.fp = parse_result.get_param("fp")
-        self.pbk = parse_result.get_param("pbk")
-        self.sid = parse_result.get_param("sid")
-        self.spx = parse_result.get_param("spx")
-        self.flow = parse_result.get_param("flow")
-        self.path = parse_result.get_param("path", "/")
-        self.host = parse_result.get_param("host")
-        self.service_name = parse_result.get_param("serviceName")
-        self.alpn = parse_result.get_param("alpn")
-        self.mode = parse_result.get_param("mode")
+        self.type = proxy.get_param("type", "tcp")
+        self.security = proxy.get_param("security", "none")
+        self.sni = proxy.get_param("sni")
+        self.fp = proxy.get_param("fp")
+        self.pbk = proxy.get_param("pbk")
+        self.sid = proxy.get_param("sid")
+        self.spx = proxy.get_param("spx")
+        self.flow = proxy.get_param("flow")
+        self.path = proxy.get_param("path", "/")
+        self.host = proxy.get_param("host")
+        self.service_name = proxy.get_param("serviceName")
+        self.alpn = proxy.get_param("alpn")
+        self.mode = proxy.get_param("mode")
 
-        self.extra = parse_result.get_extra_params(self.known_params)
+        self.extra = proxy.get_extra_params(self.known_params)
 
-    def get_outbound(self):
+    def get_outbound(self) -> dict:
         user = {
-            "id": self.uuid,
+            "id": self.proxy.username,
             "encryption": "none",
             "level": 0
         }
@@ -41,13 +37,13 @@ class VlessProxy:
             user["flow"] = self.flow
 
         outbound = {
-            "tag": self.tag,
+            "tag": self.proxy.tag,
             "protocol": "vless",
             "settings": {
                 "vnext": [
                     {
-                        "address": self.server,
-                        "port": self.port,
+                        "address": self.proxy.server,
+                        "port": self.proxy.port,
                         "users": [user]
                     }
                 ]
@@ -124,5 +120,144 @@ class VlessProxy:
             outbound["streamSettings"]["httpupgradeSettings"] = httpupgrade_settings
 
 
+
+        return outbound
+
+from service.proxy import Proxy
+
+
+class VlessProxy:
+    known_params = {
+        "type", "security", "sni", "fp", "pbk", "sid", "spx", "flow",
+        "path", "host", "serviceName", "alpn", "mode"
+    }
+
+    def __init__(self, proxy: Proxy):
+        self.proxy = proxy
+
+    def get_outbound(self):
+        proxy = self.proxy
+
+        network = proxy.get_param("type", "tcp")
+        security = proxy.get_param("security", "none")
+        flow = proxy.get_param("flow")
+
+        user = {
+            "id": proxy.username,
+            "encryption": "none",
+            "level": 0
+        }
+
+        if flow:
+            user["flow"] = flow
+
+        outbound = {
+            "tag": proxy.tag,
+            "protocol": "vless",
+            "settings": {
+                "vnext": [
+                    {
+                        "address": proxy.server,
+                        "port": proxy.port,
+                        "users": [user]
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": network,
+                "security": security
+            }
+        }
+
+        if security == "tls":
+            tls_settings = {}
+
+            sni = proxy.get_param("sni")
+            fp = proxy.get_param("fp")
+            alpn = proxy.get_param("alpn")
+
+            if sni:
+                tls_settings["serverName"] = sni
+
+            if fp:
+                tls_settings["fingerprint"] = fp
+
+            if alpn:
+                tls_settings["alpn"] = [alpn]
+
+            outbound["streamSettings"]["tlsSettings"] = tls_settings
+
+        elif security == "reality":
+            sni = proxy.get_param("sni")
+            pbk = proxy.get_param("pbk")
+            sid = proxy.get_param("sid")
+            fp = proxy.get_param("fp")
+            spx = proxy.get_param("spx")
+            path = proxy.get_param("path", "/")
+
+            outbound["streamSettings"]["realitySettings"] = {
+                "serverName": sni,
+                "publicKey": pbk,
+                "shortId": sid,
+                "fingerprint": fp,
+                "spiderX": spx or path or "/"
+            }
+
+        if network == "ws":
+            path = proxy.get_param("path", "/")
+            host = proxy.get_param("host")
+
+            ws_settings = {
+                "path": path or "/"
+            }
+
+            if host:
+                ws_settings["headers"] = {
+                    "Host": host
+                }
+
+            outbound["streamSettings"]["wsSettings"] = ws_settings
+
+        elif network == "grpc":
+            service_name = proxy.get_param("serviceName")
+            mode = proxy.get_param("mode")
+
+            outbound["streamSettings"]["grpcSettings"] = {
+                "serviceName": service_name or "",
+                "multiMode": mode == "multi"
+            }
+
+        elif network == "http":
+            path = proxy.get_param("path", "/")
+            host = proxy.get_param("host")
+
+            outbound["streamSettings"]["httpSettings"] = {
+                "path": path or "/",
+                "host": [host] if host else []
+            }
+
+        elif network == "quic":
+            outbound["streamSettings"]["quicSettings"] = {
+                "security": "none",
+                "key": "",
+                "header": {
+                    "type": "none"
+                }
+            }
+
+        elif network == "httpupgrade":
+            path = proxy.get_param("path", "/")
+            host = proxy.get_param("host")
+
+            httpupgrade_settings = {
+                "path": path or "/"
+            }
+
+            if host:
+                httpupgrade_settings["host"] = host
+
+            outbound["streamSettings"]["httpupgradeSettings"] = (
+                httpupgrade_settings
+            )
 
         return [outbound]
--- service/proxy.py
+++ service/proxy.py
@@ -1,6 +1,24 @@
 from urllib.parse import urlparse, parse_qs, unquote
 import service
+from protocol.vless import VlessProxy
+from protocol.vmess import VmessProxy
+from protocol.trojan import TrojanProxy
+from protocol.shadowsocks import ShadowsocksProxy
+from protocol.hysteria2 import Hysteria2Proxy
+from protocol.http import HttpProxy
 
+
+PROTOCOL_REGISTRY = {
+    "vless": VlessProxy,
+    "vmess": VmessProxy,
+    "trojan": TrojanProxy,
+    "ss": ShadowsocksProxy,
+    "shadowsocks": ShadowsocksProxy,
+    "hysteria2": Hysteria2Proxy,
+    "hy2": Hysteria2Proxy,
+    "http": HttpProxy,
+    "https": HttpProxy,
+}
 class Proxy:
     protocol: str
     server: str
@@ -9,6 +27,7 @@ class Proxy:
     password: str
     tag: str
     extra_params: list
+    query: dict
     structure: dict
 
     def __init__(self, protocol, server, port, username=None, password=None, tag=None, extra_params=None):
@@ -20,41 +39,79 @@ class Proxy:
         self.password = password
         self.tag = tag
 
-        self.extra_params = extra_params
+        self.query = extra_params
 
         self.structure = {
             "log":{
                 "loglevel": "warning"
             },
             "inbounds": service.inbounds.get_inbounds(),
-            "outbounds": ...,
-            "routing": ...
+            "outbounds": build_outbound()
+            "routing": {}
         }
 
     @classmethod
-    def from_URL(cls, URL: str):
-        P = urlparse(URL)
+    def get_system_outbounds() -> list[dict]:
+        return [
+            {
+                "tag": "direct",
+                "protocol": "freedom",
+                "settings": {},
 
-        return cls(protocol=P.scheme, server=P.hostname, port=P.port, username=P.username, password=P.password, tag=P.fragment, extra_params=None)
+            {
+                "tag": "block",
+                "protocol": "blackhole",
+                "settings": {},
+            },
+        ]
 
     @classmethod
-    def from_file(cls, path:Path):
+    def from_URL(cls, URL: str) -> cls:
+        P = urlparse(URL)
+
+        query = {k: v[0] for k, v in parse_qs(parsed.query).items()}
+
+        return cls(protocol=P.scheme, server=P.hostname, port=P.port, username=P.username,
+                   password=P.password, tag=P.fragment, extra_params=query)
+
+    @classmethod
+    def from_file(cls, path:Path) -> cls:
         with open(path, "r", encoding="utf-8") as file:
             configuration = json.load(file)
 
         return cls.from_configuration(configuration)
 
-    def from_configuration(cls, configuration: dict):
+    def from_configuration(cls, configuration: dict) -> cls:
         ...
 
         # extract args
         # return cls(*args)
 
-    def get_param(self, key: str, default=None):
+    def get_param(self, key: str, default=None) -> str:
         return self.query.get(key, default)
 
-    def get_extra_params(self, known_params: list):
+    def get_extra_params(self, known_params: list) -> dict:
         return{ k: v 
                 for k, v in self.query.items()
                 if k not in known_params
             }
+
+    def get_proxy_class(self):
+        proxy_class = PROTOCOL_REGISTRY.get(self.protocol.lower())
+
+        if proxy_class is None:
+            raise ValueError(f"Unsupported protocol: {scheme}")
+
+        return proxy_class
+
+    def build_outbound(self) -> list:
+        proxy_class = get_proxy_class(p.protocol)
+
+        proxy = proxy_class(self)
+
+        outbound = []
+
+        outbound.extend(proxy.get_outbound())
+        outbound.extend(get_system_outbounds())
+
+        return outbound
